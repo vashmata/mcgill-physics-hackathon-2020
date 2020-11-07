6f82ccaadc67bf475e3980c8f685238cd4637ee0
cferrus <68361708+cferrus@users.noreply.github.com>
2020-11-07 16:00:31 -0500

    Update and rename formulas to formulas.py

---
+++ formulas.py
@@ -0,0 +1,10 @@
+
+def displacement (x, v, a, t): 
+    answer=x+v*t+(1/2*(a)*t**2)
+    return answer
+def slope (m,x,b):
+    answer = mx+b
+    return answer
+def force (roh,coef,A,v):
+    answer = 1/2*coef*roh*A*v**2
+    return answer
